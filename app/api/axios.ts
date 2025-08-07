9d3325f712f74753e6d74171b16cd5d96bdbe3d8
Aakash <rvaakashraj@gmail.com>
2025-08-07 09:38:02 +0530

    razorpay code added

--- app/api/axios.ts
+++ app/api/axios.ts
@@ -9,7 +9,7 @@ const axiosInstance = axios.create({
   // https://bookmycuts.onrender.com
   // http://localhost:3002/api/
   // https://e4d54b45f914.ngrok-free.app 
-  baseURL: 'https://e4d54b45f914.ngrok-free.app/api/', // ⬅️ Replace with your real API base URL
+  baseURL: 'https://bookmycuts.onrender.com/api/', // ⬅️ Replace with your real API base URL
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
